43a7afc666327c20b2c34de014a5c798849fbf04
FurqanMujahid <furqanfurqan7777@gmail.com>
2024-11-27 16:29:58 +0500

    Add project files.

---
+++ ToDoReactOrCore/Controllers/ToDoController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ToDoReactOrCore.Data;
+using ToDoReactOrCore.Models;
+
+namespace ToDoReactOrCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+
+        private readonly AppDataContext _context;
+
+        public ToDoController(AppDataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetTodos()
+        {
+            return Ok(_context.TodoItems.ToList());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetTodoById(int id)
+        {
+            var todo = _context.TodoItems.Find(id);
+            if (todo == null) return NotFound();
+            return Ok(todo);
+        }
+
+        [HttpPost]
+        public IActionResult CreateTodo([FromBody] TodoItem todo)
+        {
+            _context.TodoItems.Add(todo);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetTodoById), new { id = todo.Id }, todo);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateTodo(int id, [FromBody] TodoItem updatedTodo)
+        {
+            var todo = _context.TodoItems.Find(id);
+            if (todo == null) return NotFound();
+
+            todo.TaskTitle = updatedTodo.TaskTitle;
+            todo.IsComplete = updatedTodo.IsComplete;
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteTodo(int id)
+        {
+            var todo = _context.TodoItems.Find(id);
+            if (todo == null) return NotFound();
+
+            _context.TodoItems.Remove(todo);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
---
+++ ToDoReactOrCore/Data/AppDataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoReactOrCore.Models;
+
+namespace ToDoReactOrCore.Data
+{
+    public class AppDataContext : DbContext
+    {
+        public AppDataContext(DbContextOptions<AppDataContext> options) : base(options) { }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
